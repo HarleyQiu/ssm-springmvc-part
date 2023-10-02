2436d3dcca7aa6275d2cf9b46f70fa3b2d55f617
QiuLinhao <210554114@qq.com>
2023-10-02 13:14:22 +0800

    拦截器的告诫使用 和 参数校验

--- springmvc-high-other-05/src/main/java/org/example/config/MvcConfig.java
+++ springmvc-high-other-05/src/main/java/org/example/config/MvcConfig.java
@@ -1,6 +1,7 @@
 package org.example.config;
 
 import org.example.interceptor.MyInterceptor;
+import org.example.interceptor.MyInterceptor1;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.*;
@@ -25,9 +26,17 @@ public class MvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        // 拦截全部请求
-        registry.addInterceptor(new MyInterceptor());
+        // 1. 拦截全部请求
+        // registry.addInterceptor(new MyInterceptor());
 
-        //
+        // 2. 指定地址拦截 .addPathPatterns("/user/data");
+        // *  任意一层字符串   ** 任意多层字符串
+        // registry.addInterceptor(new MyInterceptor()).addPathPatterns("/user/**");
+
+        // 3. 排除拦截 排除的地址应该在拦截地址内部
+        // registry.addInterceptor(new MyInterceptor()).addPathPatterns("/user/**").excludePathPatterns("/user/data1");
+
+        registry.addInterceptor(new MyInterceptor());
+        registry.addInterceptor(new MyInterceptor1());
     }
 }
--- springmvc-high-other-05/src/main/java/org/example/controller/UserController.java
+++ springmvc-high-other-05/src/main/java/org/example/controller/UserController.java
@@ -1,13 +1,46 @@
 package org.example.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.example.pojo.User;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
+
+    //接收用户数据。用户有校验注解
+
+    /**
+     * 步骤1： 实体类属性添加校验注解
+     * 步骤2： handler(@validated实体类 对象)
+     * 细节：  param | json 校验注解都有效果
+     * json参数 - @RequestBody
+     * <p>
+     * 如果，不符合校验规则，直接向前端抛出异常！
+     * 接收错误绑定信息！自定义返回结果！  约定： 参数错误 -》 {code:400}  -》前端
+     * 捕捉错误绑定错误信息：
+     * 1. handler(校验对象,BindingResult result) 要求： bindingResult必须紧挨着 校验对象
+     * 2. BindingResult获取绑定错误
+     */
+    @PostMapping("register")
+    public Object register(@Validated @RequestBody User user, BindingResult result) {
+
+        if (result.hasErrors()) {
+            //有绑定错误，就不直接返回了，由我们自己决定！
+            Map<String, Object> data = new HashMap<>();
+            data.put("code", 400);
+            data.put("msg", "参数校验异常了！");
+            return data;
+        }
+
+        System.out.println("user = " + user);
+        return user;
+    }
 
     @GetMapping("/data")
     public String data() {
